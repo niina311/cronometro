5c3692a63a955ec9f8cd43bbc0799009ee4aa1e2
niina311 <marina.cardoso.rodrigues@escola.pr.gov.br>
2026-06-01 11:38:44 +0000

    aula 4 reviao e erros

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
@@ -8,8 +11,4 @@ for (let i = 0; i < botoes.length; i++) {
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
   };
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
-
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+}
